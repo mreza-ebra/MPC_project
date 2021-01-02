62574cc769e32a9a4133e92c3e15183cb5b657b2
mreza-ebra <mohammadrza.ebrahimi@gmail.com>
2021-01-02 16:36:44 +0100

    completed part1 and 2

--- src/initial_sim.m
+++ src/initial_sim.m
@@ -1,7 +1,7 @@
 quad = Quad();
 Tf = 1.0;
 x0 = zeros(12,1);
-u = [1;0.5;1;1];
+u = [0.7007;0.7007;0.7007;0.7007];
 
 sim = ode45(@(t,x) quad.f(x,u), [0, Tf], x0);
 quad.plot(sim, u);
--- src/lin_diag.m
+++ src/lin_diag.m
@@ -1,3 +1,7 @@
+clear all;
+clc;
 quad = Quad();
-[xs,us] = quad.trim()        % Compute steady−state for which 0 = f(xs,us)
-sys = quad.linearize(xs, us) % Linearize the nonlinear model
+[xs,us] = quad.trim();        % Compute steady−state for which 0 = f(xs,us)
+sys = quad.linearize(xs, us); % Linearize the nonlinear model
+sys_transformed = sys * inv(quad.T); % New system is A * x + B * inv(T) * v
+[sys_x, sys_y, sys_z, sys_yaw] = quad.decompose(sys, xs, us);
